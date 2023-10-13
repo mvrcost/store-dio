c9ff34c896a98e034ee8f00a9ea9f6a22bd45ff4
Marcos Nascimento <131052793+mvrcost@users.noreply.github.com>
2023-10-12 22:21:01 -0300

    Initial commit

---
+++ src/app/components/card/card-price/card-price.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-price',
+  templateUrl: './card-price.component.html',
+  styleUrls: ['./card-price.component.css']
+})
+export class CardPriceComponent {
+  @Input()gameType = ''
+  @Input()gamePrice = ''
+
+
+
+}
